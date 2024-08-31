24a31f19fd0bdb06659b4e16c557c8384984b932
Mohith Gowda K <mohithgowdak.kote@gmail.com>
2024-08-31 10:33:22 +0530

    final

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # this is to import user database
 # Create your models here.
 
 class Signup(models.Model):
